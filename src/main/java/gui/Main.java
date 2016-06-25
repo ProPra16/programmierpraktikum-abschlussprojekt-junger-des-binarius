529ff7eb2df8a37fdc2759b89b87aa8f3e6f12c1
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2016-06-25 19:04:49 +0200

    Main.java als Application gemacht

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,11 +1,26 @@
 package gui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by cansc on 23.06.2016.
  */
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/layout.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("TDDT");
+        primaryStage.show();
     }
 }
