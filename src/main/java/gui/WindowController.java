1fd83381b39d32a02c58f6f623dffc9affa317de
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2016-06-27 16:30:33 +0200

    Controller und layout erweitert

--- src/main/java/gui/WindowController.java
+++ src/main/java/gui/WindowController.java
@@ -1,5 +1,19 @@
 package gui;
 // Created by User on 25.06.2016.
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class WindowController {
+    @FXML
+    private Label statusPassed, statusFailed, statusRed, statusGreen, statusRefactor;
+    @FXML
+    private Button switchToGreen, switchToRefactor, switchToRed;
+    @FXML
+    private TextArea codeArea;
+    public void openListView(ActionEvent actionEvent) {
+    }
 }
