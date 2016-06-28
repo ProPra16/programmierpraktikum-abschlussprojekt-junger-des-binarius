ffb21e8832ffd1289de335d751feac3dabb84e29
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2016-06-28 15:50:54 +0200

    Zeigt exercises in ListView an

--- src/main/java/gui/ListViewWindow.java
+++ src/main/java/gui/ListViewWindow.java
@@ -2,35 +2,65 @@ package gui;
 // Created by User on 27.06.2016.
 
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import system.Catalog;
+import system.Exercise;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+
 
 public class ListViewWindow {
-    public static void createWindow(){
+    private Exercise selectedExercise;
+    //Erstellt ListView-Fenster
+    public void createWindow(){
         VBox listPane = new VBox();
+        Label label = new Label();
         ListView<String> exerciseView = new ListView<>();
-        ObservableList<String> exerciseList = FXCollections.observableArrayList("Aufgabe 1", "Aufgabe 2", "Aufgbae 3");
-        exerciseView.setItems(exerciseList);
+        Exercise[] exercises = loadExercises();
+        for(Exercise exercise : exercises){
+            exerciseView.getItems().add(exercise.getName());
+        }
+
         Button load = new Button("Load Exercise");
+        load.setOnAction((ActionEvent event) -> {
+            int index = exerciseView.getFocusModel().getFocusedIndex();
+
+            label.setText(index+"");
+        });
+
         listPane.getChildren().add(exerciseView);
         listPane.getChildren().add(load);
+        listPane.getChildren().add(label);
         Scene listScene = new Scene(listPane);
         Stage listStage = new Stage();
+        //Fenster
         listStage.initModality(Modality.WINDOW_MODAL);
         listStage.initOwner(Main.primaryStage);
         listStage.setScene(listScene);
         listStage.show();
 
+    }
+
+    private Exercise[] loadExercises(){
+        Catalog catalog = new Catalog();
+        catalog.loadCatalogFromXML();
+        return catalog.getExercises();
+    }
+
+    private void chooseExercise(int index){
+        Catalog catalog = new Catalog();
+        selectedExercise = catalog.getExercise(index);
+    }
+
+    public Exercise getSelectedExercise() {
+        return selectedExercise;
     }
 }
