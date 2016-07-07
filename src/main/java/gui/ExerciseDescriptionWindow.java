36bf0bc70bb50bdc34923e8c53e727096ccad65b
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2016-07-07 17:50:25 +0200

    Nils: Icon fuer DescriptionWindow

--- src/main/java/gui/ExerciseDescriptionWindow.java
+++ src/main/java/gui/ExerciseDescriptionWindow.java
@@ -1,8 +1,8 @@
 package gui;
-// Created by User on 07.07.2016.
 
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import system.Exercise;
@@ -11,9 +11,13 @@ import java.net.URL;
 
 public class ExerciseDescriptionWindow {
 
+    private static final Image icon = new Image("images/ICON_Catalog.png");
+
     public static void createWindow(Exercise exercise){
         URL stylesheet = Main.class.getResource("/gui/TDDT_style.css");
         Stage stage = new Stage();
+        stage.getIcons().setAll(icon);
+        stage.setResizable(false);
         VBox vBox = new VBox();
         vBox.getStylesheets().add(stylesheet.toExternalForm());
         TextArea descriptionArea = new TextArea();
